60b585bf949f3583c84539cde995f6fe46cbf54b
height <divcssphp@gmail.com>
2018-06-15 15:46:26 +0800

    基础布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -8,3 +8,6 @@
 function test_helper(){
     return 'OK';
 }
+function route_class(){
+    return str_replace('.','-',Route::currentRouteName());
+}
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title','shouye')
+
+@section('content')
+<h2>xxxxx</h2>
+@stop
