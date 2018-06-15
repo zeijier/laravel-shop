04bd5d0d47dd2411235d77e0b2ae62ad280a1af1
height <divcssphp@gmail.com>
2018-06-15 16:46:56 +0800

    邮箱验证中间件

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('root','PagesController@root');
+Route::get('root','PagesController@root')->name('root');
 Auth::routes();
+Route::group(['middleware'=>'auth'],function (){
+    Route::get('email_verifiy_notic','PagesController@emailVerifyNotice')->name('email_verifiy_notic');
+//    开始验证邮箱是否激活
+    Route::group(['middleware'=>'email_verified'],function (){
+        Route::get('test',function (){
+            return 'Your email is verified';
+        });
+    });
+//    结束
+});
