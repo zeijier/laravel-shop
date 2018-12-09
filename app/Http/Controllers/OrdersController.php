70c9fe17e00060e25629d7f95dd5534b2b8b4c9f
height <divcssphp@gmail.com>
2018-12-09 23:20:04 +0800

    关闭为支付订单

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\OrderRequest;
+use App\Jobs\CloseOrder;
 use App\Models\Order;
 use App\Models\ProductSku;
 use App\Models\UserAddress;
@@ -59,7 +60,7 @@ class OrdersController extends Controller
             $user->carItem()->whereIn('product_sku_id',$skuIds)->delete();
             return $order;
         });
-
+        $this->dispatch(new CloseOrder($order,config('app.order_ttl')));
         return $order;
     }
 }
